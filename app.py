a79fd1f476ea64e0338ace021be07020e6023a13
DeZer04 <charisma.deo@mhs.amikomsolo.ac.id>
2023-06-12 15:43:36 +0700

    modified:   .gitignore 	modified:   app.py 	new file:   recommendation.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -6,39 +6,54 @@ import mysql.connector
 from flask import Flask
 from google.cloud import storage
 from sklearn.model_selection import train_test_split
+import csv
+
+# Set up Flask app
+app = Flask(__name__)
+
+# Database configuration
+DB_HOST = os.environ.get('_DB_HOST')
+DB_USER = os.environ.get('_DB_USER')
+DB_PASSWORD = os.environ.get('_DB_PASSWORD')
+DB_NAME = os.environ.get('_DB_NAME')
+
+db = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+
+# Cloud Storage configuration
+BUCKET_NAME = 'project-imgs'
+
+# Filename
+projek = 'project.csv'
+ratings = 'ratings.csv'
+preferensi = 'user.csv'
 
 # Load the SavedModel
 model = tf.keras.models.load_model("model/recommendation.h5")
 
+
 # Set up Google Cloud Storage client
-secret_key = os.environ['GCP_SA_KEY']
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = secret_key
+credent = os.environ.get('_GCP_CR')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credent
 client = storage.Client()
 
-bucket_name = 'dataset-csv'
-file_name1 = 'project.csv'
-file_name2 = 'ratings.csv'
-file_name3 = 'user.csv'
-
 def download_blob(file_name, file_path):
-    bucket = client.get_bucket(bucket_name)
+    bucket = client.get_bucket(BUCKET_NAME)
     blob = bucket.blob(file_name)
     blob.download_to_filename(file_path)
 
 def insert_recommendations(user_id, recommended_projects):
-    db = mysql.connector.connect(
-        host=os.environ['DB_HOST'],
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASSWORD'],
-        database=os.environ['DB_NAME']
-    )
+    
     cursor = db.cursor()
 
     # Delete existing recommendations for the user
     delete_query = "DELETE FROM recommendations WHERE id_user = %s"
     delete_values = (user_id,)
     cursor.execute(delete_query, delete_values)
-
 
     for index, row in recommended_projects.iterrows():
         project_id = row['Idproject']
@@ -53,13 +68,6 @@ def insert_recommendations(user_id, recommended_projects):
     db.commit()
 
 def get_recommendations(ratings_data, project_data, user_data, model):
-    # Split data into training and validation sets
-    train_ratings, val_ratings = train_test_split(ratings_data, test_size=0.2, random_state=42)
-
-    # Get the number of users and projects
-    num_users = ratings_data['Iduser'].nunique()
-    num_projects = ratings_data['Idproject'].nunique()
-
     # Mapping dictionaries
     user_to_index = {user_id: index for index, user_id in enumerate(ratings_data['Iduser'].unique())}
     project_to_index = {project_id: index for index, project_id in enumerate(ratings_data['Idproject'].unique())}
@@ -84,23 +92,77 @@ def get_recommendations(ratings_data, project_data, user_data, model):
             user_indices = np.full(len(filtered_projects), user_to_index[user_id])
             project_indices = filtered_projects['Idproject'].map(project_to_index).values
 
-            predicted_ratings = model.predict([user_indices, project_indices]).flatten()
+            if len(filtered_projects) > 0:
+                # Lakukan prediksi rating menggunakan model
+                predicted_ratings = model.predict([user_indices, project_indices]).flatten()
 
-            filtered_projects['predicted_rating'] = predicted_ratings
+                # Gabungkan proyek yang belum dilihat dengan prediksi ratingnya
+                filtered_projects['predicted_rating'] = predicted_ratings
 
-            recommended_projects = filtered_projects.sort_values('predicted_rating', ascending=False).head(10)
+                # Urutkan proyek berdasarkan prediksi rating secara menurun
+                recommended_projects = filtered_projects.sort_values('predicted_rating', ascending=False).head(10)
 
-            insert_recommendations(user_id, recommended_projects)
+                # insert rekomendasi proyek
+                insert_recommendations(user_id, recommended_projects)
+                print("Recommendation succesfull")
+            else:
+                print("No projects available for recommendation.")
         else:
             print(f"Invalid pref_categories for User ID {user_id}. Skipping recommendation.")
 
-app = Flask(__name__)
+# Routes
+@app.route('/')
+def convert_to_csv():
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch the data from projects table
+    query_projek = "SELECT projects.id_proyek AS Idproject, projects.nm_proyek AS projecttitle, categories.nm_kategori AS categories FROM projects INNER JOIN categories ON projects.id_kategori = categories.id_kategori;"
+    cursor.execute(query_projek)
+    data_projek = cursor.fetchall()
+
+    # Execute the SQL query to fetch the data from ratings table
+    query_ratings = "SELECT ratings.id_proyek AS Idproject, users.id_user AS Iduser, ratings.nilai AS ratings, UNIX_TIMESTAMP(ratings.updatedAt) AS timestamp FROM ratings INNER JOIN users ON ratings.username = users.username"
+    cursor.execute(query_ratings)
+    data_ratings = cursor.fetchall()
+
+    query_pref = "SELECT id_user AS Iduser, pref_categories AS pref_categories FROM users"
+    cursor.execute(query_pref)
+    data_user = cursor.fetchall()
+
+    # Export data to a CSV file with headers
+    
+    with open(projek, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Idproject', 'projecttitle', 'categories'])  # Add header row
+        csv_writer.writerows(data_projek)
+
+    with open(ratings, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Idproject', 'Iduser', 'ratings', 'timestamp'])  # Add header row
+        csv_writer.writerows(data_ratings)
+
+    with open(preferensi, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Iduser', 'pref_categories'])  # Add header row
+        csv_writer.writerows(data_user)
+
+    # Upload the CSV files to Cloud Storage
+    bucket = client.bucket(BUCKET_NAME)
+    blob1 = bucket.blob(projek)
+    blob2 = bucket.blob(ratings)
+    blob3 = bucket.blob(preferensi)
+    blob1.upload_from_filename(projek)
+    blob2.upload_from_filename(ratings)
+    blob3.upload_from_filename(preferensi)
+
+    return 'Database converted to CSV and uploaded to Cloud Storage.'
+
 
 @app.route('/recommendations', methods=['POST'])
 def recommend_projects():
-    download_blob(file_name1, 'project.csv')
-    download_blob(file_name2, 'ratings.csv')
-    download_blob(file_name3, 'user.csv')
+    download_blob(projek, 'project.csv')
+    download_blob(ratings, 'ratings.csv')
+    download_blob(preferensi, 'user.csv')
 
     ratings_data = pd.read_csv('ratings.csv')
     project_data = pd.read_csv('project.csv')
@@ -109,6 +171,7 @@ def recommend_projects():
     get_recommendations(ratings_data, project_data, user_data, model)
 
     return "Recommendations inserted into the database successfully!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
